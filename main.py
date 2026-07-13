a7aa72d817a1ca56cea4d22abc32ee43bdb0d758
Baileybk2 <baileybk2@gmail.com>
2026-07-13 16:23:18 -0400

    player class and game class

--- main.py
+++ main.py
@@ -113,3 +113,78 @@ class Grid:
                 return True
 
         return False
+
+
+# player info
+
+class Player:
+    def __init__(self, name, pieceColor):
+        self._name = name
+        self._pieceColor = pieceColor
+
+    def getName(self):
+        return self._name
+
+    def getPieceColor(self):
+        return self._pieceColor
+
+
+# the game class will be used to play the game
+# it will keep track of the players, the score, and the grid
+class Game:
+    def __init__(self, grid, connectN, targetScore):
+        self._grid = grid
+        self._connectN = connectN
+        self._targetScore = targetScore
+
+        self._players = [
+            Player('Player 1', GridPosition: YELLOW),
+            Player('Player 2', GridPosition: RED)
+        ]
+
+        self._score = {}
+        for player in self._players:
+            self._score[player.getName()] = 0
+
+    def printBoard(self):
+        print('Board:/n')
+        grid = self._grid.getGrid():
+        for i in range(len(grid)):
+            row = ''
+            for piece in grid[i]:
+                if piece == GirdPosition.EMPTY:
+                    row += '0 '
+                elif piece == GrisPosition.YELLOW:
+                    row += 'Y '
+                elif piece == GridPosition.RED:
+                    row += 'R '
+            print(row)
+        print('')
+
+    def playMove(self, player):
+        self.printBoard()
+        print(f"{player._getName()}'s turn")
+        colCnt = self._grid.getColumnCount()
+        moveColumn = int(input(f"Enter a number between {0} and {colCnt - 1} to add piece: "))
+        moveRow = self._grid.placePiece(moveColumn, player.getPieceColor())
+        return (moveRow, moveColumn)
+
+    def playRound(self):
+        while True:
+            for player in self._players:
+                row, col = self.playMove(player)
+                pieceColor = player.getPieceColor()
+                if self._grid.checkWin(self._connectN, row, col, pieceColor):
+                    self._score[player.getName()] += 1
+                    return player
+
+    def play(self):
+        maxScore = 0
+        winner = None
+        while maxScore < _self.targetScore:
+            winner = self.playRound()
+            print(f"{winner.getName()} won the round!")
+            maxScore = max(self._score[winner.getName()], maxScore)
+
+            self._grid.initGrid()
+        print(f"{winner.getName} won the game!")
